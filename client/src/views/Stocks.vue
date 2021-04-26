47db0f83d88e7dd48ddfd90a9caa9a2730b8e23a
Nickolas Whitman <nmwhitman@knox.edu>
2021-04-26 12:54:12 -0500

    added stocks.vue to navBar.vue and updated posts.py

---
+++ client/src/views/Stocks.vue
@@ -0,0 +1,68 @@
+<template lang="html">
+  <div class="Stocks">
+    <h1>Stock News</h1>
+      <ol v-for="artic in articles" :key="artic.link">
+      <img :src= artic.image 
+        width="800" 
+        height="500"
+      />
+      <br> 
+      <a v-bind:href= artic.link>Link</a>
+      <br> <br> 
+        <h2>
+        {{ artic.wing }} 
+        </h2>
+      <br>
+        <div style="margin-left: 1em; line-height:2;">
+        {{ artic.text }}
+        </div>
+     
+      <br> <br> <br>
+      </ol>
+  </div>
+</template>
+
+
+<script>
+import PostsService from '@/services/PostsService'
+export default {
+    name: 'Stocks',
+
+    data() {
+        return {
+          articles: null
+        }
+    },
+  mounted() {
+    PostsService.get_articles("stocks")
+      .then(response => {
+        this.articles = response.data
+      })
+    }
+  }
+
+
+</script>
+
+
+<style scoped lang="css">
+
+h1 {
+  text-align: center;
+  border: 15px solid green;
+  color:rgb(230, 228, 228);
+  background-color: rgb(29, 29, 29);
+}
+h2 {
+  text-align: center;
+}
+ol {
+  padding-left: 50px;
+  padding-right: 50px;
+  text-align:left;
+  color:rgb(255, 253, 253);
+  background-color: rgb(29, 29, 29);
+  border: 2px solid black;
+}
+
+</style>
