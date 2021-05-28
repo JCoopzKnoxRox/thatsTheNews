7af301e3cf6eec9dba8338344aff4cebefaa6448
Justin Cooper <>
2021-05-28 15:36:12 -0500

    Trying to havec changes updated fro deployment (test)

--- client/deploy.sh
+++ client/deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:NickolasDesigns/frontend.git master:gh-pages
+git push -f git@github.com:JCoopzKnoxRox/thatsTheNews.git master:gh-pages
 cd -
